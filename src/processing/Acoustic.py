d389d2efdb173a1b5f73c4685ab17cb35590a697
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-08-17 00:26:34 +0200

    Working wrapper class and first version of acoustic class

--- src/processing/Acoustic.py
+++ src/processing/Acoustic.py
@@ -7,18 +7,106 @@ from scipy.fft import fft, fftfreq, fftshift
 from SES_tag.utils.acoustic_utils import *
 from SES_tag.wrapper import Wrapper
 
-class Acoustic(Wrapper):
 
-	def __init__(self):
-		
-		default = {window_size = 1024, overlap = 20, nfft = 1024}
+class Acoustic(Wrapper):
+	"""
+	Acoustic class for processing and analyzing acoustic data.
+
+	This class provides methods to normalize acoustic data and compute the noise level
+	using windowed Fourier transforms. It is initialized with a timestamp and parameters
+	for data and spectrogram normalization.
+	"""
+
+	def __init__(self, timestamp, wav_path = None, *, data_normalization : str = 'instrument', spectro_normalization : str = 'density', **kwargs):
+		"""
+		Initializes the Acoustic class.
+
+		Parameters
+		----------
+		timestamp: dataframe-like object
+			A timestamp object, expected to contain file information for loading acoustic data. Stores beginning timestamp of each wavfile.
+			Contains columns fn (filename)
+		data_normalization : 'str', optional
+			Type of data normalization to be applied. Default is 'instrument'.
+		spectro_normalization : 'str', optional
+			Type of spectrogram normalization to be applied. Default is 'density'.
+		**kwargs: Additional parameters like 'window_size' (bins), 'nfft' (bins), and 'duration' (seconds).
+
+		Attributes:
+		self.timestamp: Stores wavefiles names and begin POSIX timestamps.
+		self.samplerate: Sample rate extracted from the first file in the timestamp.
+		self.params: Dictionary of parameters for window size, FFT size, offset and others.
+		self.data_normalization: Method for data normalization.
+		self.spectro_normalization: Method for spectrogram normalization.
+		"""
+		self.timestamp = timestamp
+		self.wav_path = wav_path if wav_path else os.getcwd()
+		self.samplerate = sf.info(self.timestamp.fn.iloc[0]).samplerate
+		offset = self.dt * self.samplerate
+		default = {window_size = 1024, nfft = 1024, offset = offset}
 		self.params = {**default, **kwargs}
+		if 'duration' in self.params.keys():
+			self.params['window_size'] = self.params['duration'] * self.samplerate
 		self.data_normalization = data_normalization
 		self.spectro_normalization = spectro_normalization
-		pass
 
-	def normalize(self):
+	def __call__(self):
+		return self.forward()
 
+	def forward(self, overwrite = False) :
+		"""
+		Processes acoustic data files, normalizes them, computes noise levels, and updates a NetCDF dataset with the results.
+		Parameters
+		----------
+		overwrite : bool
+		A flag indicating whether to overwrite existing variables in the NetCDF dataset.
+
+		Raises:
+		------
+		ValueError:
+		If frequency components (`freqs`) differ across files.
+		"""
+		final_freqs = None
+		acoustic_data = []
+		for i, row in self.timestamp.iterrows():
+			data, fs = sf.read(os.path.join(self.wav_path, row.fn))
+			data = self.normalize(data)
+			data, freqs = self.compute_noise_level(data)
+
+			if final_freqs is None:
+				final_freqs = freqs
+			else:
+				if np.array_equal(final_freqs, freqs):
+					acoustic_data.extend(data)
+				else:
+					print(f"Found different frequencies for file {row.fn}. Skipping.")
+
+		if overwrite :
+			for freq in freqs :
+				if freqs in self.ds.variables:
+					self.remove_variable(freq)
+
+		for i, freq in enumerate(final_freqs) :
+			if freq not in self.ds.variables:
+				freq_dim = self.ds.createDimension(freq, len(acoustic_data))
+				freq_var = self.ds.createVariable(freq, np.float32, (freq,))
+				freq_var.units = 'dB re 1 uPa**2 / Hz' if self.spectro_normalization == 'density' else 'dB re 1 uPa**2'
+				freq_var.db_type = 'absolute' if self.data_normalization == 'instrument' else 'relative'
+				freq_var.long_name = f'Power spectral density at {freq} Hz' if self.spectro_normalization == 'density' else 'Power spectrum at {freq} Hz'
+				freq_var[:] = acoustic_data[:, i]
+
+
+	def normalize(self, data) :
+		"""
+		Normalizes the input acoustic data based on the specified data_normalization method.
+		Parameters
+		----------
+		    data: array-like
+			The raw acoustic data to be normalized.
+		Returns
+		-------
+		    Normalized data.
+		"""
 	       if self.data_normalization == "instrument":
 		    data = (
 		        (data * self.peak_voltage)
@@ -26,33 +114,17 @@ class Acoustic(Wrapper):
 		        / 10 ** (self.gain_dB / 20))
 
 
-	def run_functions(size, step, timestamp, path, filename = 'spl_data.csv', iteration = 0):
-		'''
-		size : length of soundfile to analyze in seconds
-		step : step between two successive sound files
-		timestamp : contains beginning time of each audio file (epoch format)
-		path : path to audio files eg '/run/..../ml20_293a/raw/'
-		freqs : List of frequencies at which you want SPL to be computed
-		'''
-		timestamp = pd.read_csv(timestamp)
-		spl_dict = {**{'time':np.arange(timestamp.begin.iloc[0], timestamp.end.iloc[-1], step)}}
-		spl_dict['fns'], spl_dict['start_time'] = find_corresponding_file(timestamp, spl_dict['time'])
-		data = []
-		for i in tqdm(range(iteration*3600, len(spl_dict['fns']) - iteration*3600), position = 0, leave = True):
-			fs = sf.info(path+spl_dict['fns'][i]).samplerate
-				sig, fs = sf.read(path+spl_dict['fns'][i], start = int((spl_dict['time'][i]-spl_dict['start_time'][i])*fs), stop = int((spl_dict['time'][i]-spl_dict['start_time'][i]+size)*fs))
-			f, Pxx = sg.welch(sig, fs=fs, nperseg=2048)
-			_row = np.append(Pxx, spl_dict['time'][i])
-			data.append(_row)
-			if (i+1) % 3600 == 0:
-				df = pd.DataFrame(data)
-				df.columns = np.append(f, ['time'])
-				df.to_csv(f'{i//3600}_{filename}')
-				data = []
-
-	def osmose(self): 
-
-		Noverlap = int(self.window_size * self.overlap / 100)
+	def compute_noise_level(self, data): 
+		"""
+		Computes the noise level of the provided acoustic data using windowed Fourier transforms.
+		Parameters
+		----------
+		data: array-like
+			The raw acoustic data for which the noise level is to be computed.
+		Returns
+		-------
+		log_spectro: The computed and normalized spectrogram, representing the noise level in the data.
+		"""
 
 		win = np.hamming(self.params['window_size'])
 		if self.params['nfft'] < (self.params['window_size']):
@@ -67,15 +139,13 @@ class Acoustic(Wrapper):
 		        scale_psd = 2.0 / (win.sum() ** 2)
 
 		Nbech = np.size(data)
-		Noffset = self.params['window_size'] - Noverlap
-		Nbwin = int((Nbech - self.params['window_size']) / Noffset)
-		Freq = np.fft.rfftfreq(self.params['nfft'], d=1 / sample_rate)
+		Nbwin = int((Nbech - self.params['window_size']) / self.params['offset'])
+		freqs = np.fft.rfftfreq(self.params['nfft'], d=1 / sample_rate)
 
-		Sxx = np.zeros([np.size(Freq), Nbwin])
-		Time = np.linspace(0, Nbech / sample_rate, Nbwin)
+		Sxx = np.zeros([np.size(freqs), Nbwin])
 		for idwin in range(Nbwin):
 		    if self.nfft < (self.window_size):
-		        x_win = data[idwin * Noffset : idwin * Noffset + self.window_size]
+		        x_win = data[idwin * self.params['offset'] : idwin * self.params['offset'] + self.window_size]
 		        _, Sxx[:, idwin] = signal.welch(
 		            x_win,
 		            fs=sample_rate,
@@ -85,7 +155,7 @@ class Acoustic(Wrapper):
 		            scaling=self.params['spectro_normalization']
 		        )
 		    else:
-		        x_win = data[idwin * Noffset : idwin * Noffset + self.window_size] * win
+		        x_win = data[idwin * self.params['offset'] : idwin * self.params['offset'] + self.window_size] * win
 		        Sxx[:, idwin] = np.abs(np.fft.rfft(x_win, n=self.nfft)) ** 2
 		    Sxx[:, idwin] *= scale_psd
 
@@ -100,6 +170,6 @@ class Acoustic(Wrapper):
 		        Sxx *= self.params['window_size'] / 2  # value around 0dB
 		        log_spectro = 10 * np.log10(Sxx + (1e-20))
 
-			
+		return log_spectro, freqs
 		
 		
--- src/wrapper.py
+++ src/wrapper.py
@@ -4,11 +4,12 @@ import numpy as np
 
 class Wrapper():
 
+	dt : int = 3
+
 	def __init__(
 		self, 
 		ind : str = None,
 		path : str = None,
-		dt : int = 3,
 	):
 		"""
 		Parameters
@@ -27,45 +28,121 @@ class Wrapper():
 		assert isinstance(self.ind, str), "Please specify an individual"
 
 		#Check if individual file already exists, if not create it
-		self.ds = nc.Dataset(os.path.join(self.path, self.ind + '.nc'), mode='a' if os.path.exists(os.path.join(self.path, self.ind + '.nc')) else 'w', format='NETCDF4_CLASSIC')
-
+		mode='a' if os.path.exists(os.path.join(self.path, self.ind + '.nc')) else 'w'
+		self.ds = nc.Dataset(os.path.join(self.path, self.ind + '.nc'), mode = mode)
+		self.dataset_name
 
 	@property
 	def dataset_name(self) :
-		self.ds.title = f"Processed dataset for {self.ind}"
-		self.ds.subtitle = f"NetCDF structure storing processed data from the {self.ind} individual using a {self.dt} s timestep"
+		"""
+		Get or set the dataset title and subtitle.
+		The title and subtitle of the NetCDF dataset are set based on the instance's `ind` and `dt` attributes.
+		"""
+		title = getattr(self.ds, 'title', None)
+		if title is None:
+			self.ds.title = f"Processed dataset for {self.ind}"
+			self.ds.subtitle = f"NetCDF structure storing processed data from the {self.ind} individual using a {self.dt} s timestep"
+		return title
 
 	@dataset_name.setter
 	def dataset_name(self, title : str) :
+		"""
+		Set the dataset title.
+		Parameters
+		----------
+		title : str
+			The title to set for the dataset.
+		"""
 		self.ds.title = title
 
-	def create_time(self, time_data):
-		time_dim = self.ds.createDimension('time', len(time_data)) # unlimited axis (can be appended to)
-		time = self.ds.createVariable('time', np.float64, ('time',))
-		time.units = 'seconds since 1970-01-01 00:00:00 UTC'
-		time.long_name = 'POSIX timestamp'
-		time.calendar = 'standard'
-		time[:] = time_data
+	def create_time(self, time_data, overwrite = False):
+		"""
+		Create or update the 'time' variable in the dataset.
+		This method creates a new 'time' dimension and variable, or updates an existing one, with the provided time data.
 
-		date_dim = self.ds.createDimension('date', len(time_data))
-		date = self.ds.createVariable('date', np.float64, ('date',))
-		date.units = 'ISO formatted datetime in UTC'
-		date.long_name = 'Datetime'
-		date.calendar = 'standard'
-		date[:] = [nc.num2date(time_value, units='seconds since 1970-01-01 00:00:00 UTC').isoformat() for time_value in time_data]
-		
-	def create_gps(self, lat_data, lon_data):
-		lat_dim = self.ds.createDimension('lat', len(lat_data))     # latitude axis
-		lon_dim = self.ds.createDimension('lon', len(lon_data))    # longitude axis
-		lat = self.ds.createVariable('lat', np.float32, ('lat',))
-		lat.units = 'decimal degrees north'
-		lat.long_name = 'latitude'
-		lon = self.ds.createVariable('lon', np.float32, ('lon',))
-		lon.units = 'decimal degrees east'
-		lon.long_name = 'longitude'
+		Parameters
+		----------
+		time_data : array-like
+			The time data to store in the 'time' variable. Should be an array-like structure of float values.
+		overwrite : bool, optional
+			If True, any existing 'time' variable will be removed before creating the new one. Defaults to False.
+		"""
+		if overwrite :
+			if 'time' in self.ds.variables:
+				self.remove_variable('time')
 
-		lat[:] = lat_data
-		lon[:] = lon_data
+		if 'time' not in self.ds.variables:
+			time_dim = self.ds.createDimension('time', len(time_data)) # unlimited axis (can be appended to)
+			time = self.ds.createVariable('time', np.float64, ('time',))
+			time.units = 'seconds since 1970-01-01 00:00:00 UTC'
+			time.long_name = 'POSIX timestamp'
+			time.calendar = 'standard'
+			time[:] = time_data
+
+	def create_gps(self, lat_data, lon_data, overwrite = False):
+		"""
+		Create or update the 'lat' and 'lon' variables in the dataset.
+		This method creates new 'lat' and 'lon' dimensions and variables, or updates existing ones, with the provided latitude and longitude data.
+
+		Parameters
+		----------
+		lat_data : array-like
+			The latitude data to store in the 'lat' variable. Should be an array-like structure of float values.
+		lon_data : array-like
+			The longitude data to store in the 'lon' variable. Should be an array-like structure of float values.
+		overwrite : bool, optional
+			If True, any existing 'lat' and 'lon' variables will be removed before creating the new ones. Defaults to False.
+		"""
+		if overwrite :
+			if 'lat' in self.ds.variables:
+				self.remove_variable('lat')
+			if 'lon' in self.ds.variables:
+				self.remove_variable('lon')
+
+		if ('lat' not in self.ds.variables) & ('lon' not in self.ds.variables):
+			lat_dim = self.ds.createDimension('lat', len(lat_data))     # latitude axis
+			lon_dim = self.ds.createDimension('lon', len(lon_data))    # longitude axis
+			lat = self.ds.createVariable('lat', np.float32, ('lat',))
+			lat.units = 'decimal degrees north'
+			lat.long_name = 'latitude'
+			lon = self.ds.createVariable('lon', np.float32, ('lon',))
+			lon.units = 'decimal degrees east'
+			lon.long_name = 'longitude'
+			lat[:] = lat_data
+			lon[:] = lon_data
+
+	def remove_variable(self, var_name: str):
+		"""
+		Removes a specified variable from the NetCDF dataset.
+		This method creates a new NetCDF file with the same dimensions and attributes as the 
+		current dataset, but excludes the specified variable.
+		Parameters
+		----------
+		var_name : str
+			The name of the variable to remove from the dataset.
+		"""
+		if var_name in self.ds.variables:
+			# Create a new dataset and copy the variables excluding the one to remove
+			new_file_path = os.path.join(self.path, self.ind + '_new.nc')
+			with nc.Dataset(new_file_path, 'w', format='NETCDF4') as new_ds:
+				# Copy dimensions
+				for name, dimension in self.ds.dimensions.items():
+					if name != var_name:
+						new_ds.createDimension(name, len(dimension) if not dimension.isunlimited() else None)
+				# Copy variables except the one to be removed
+				for name, variable in self.ds.variables.items():
+					if name != var_name:
+						new_var = new_ds.createVariable(name, variable.datatype, variable.dimensions)
+						new_var[:] = variable[:]
+				# Copy global attributes
+				for attr_name in self.ds.ncattrs():
+					setattr(new_ds, attr_name, getattr(self.ds, attr_name))
+
+		# Replace old file with new file
+		self.ds.close()
+		os.rename(new_file_path, os.path.join(self.path, self.ind + '.nc'))
+		self.ds = nc.Dataset(os.path.join(self.path, self.ind + '.nc'), mode='a', format='NETCDF4')
+
 
 
 
