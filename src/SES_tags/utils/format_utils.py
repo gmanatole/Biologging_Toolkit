d726e95cbe92a1cb6f59f1db1abe46ef86f7b07f
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-08-29 10:41:49 +0200

    read correct column name from xml file

--- src/SES_tags/utils/format_utils.py
+++ src/SES_tags/utils/format_utils.py
@@ -3,11 +3,17 @@ from datetime import datetime, timezone
 import numpy as np
 import calendar
 import time
+from SES_tags.config.config import *
+import pandas as pd
 
 def get_start_time_sens(x) :
 	return datetime.strptime(x, '%Y/%m/%d %H:%M:%S').replace(tzinfo=timezone.utc).timestamp()
 
 def get_start_time_xml(path) :
+	"""
+	Looks in .xml file for keyword ACC and returns associated TIME as a POSIX timestamp.
+	Code assumes that TIME is in UTC and returns UTC timestamp
+	"""
 	if isinstance(path, (str, np.str_)):
 		tree = ET.parse(path)
 		root = tree.getroot()
@@ -50,19 +56,44 @@ def get_start_date_xml(path) :
 			xml_dates.append(get_xml_date(line))
 		return np.array(xml_dates)
 
-def get_xml_columns(path) :
-	pass
 
-import os
-
-# Get the directory of the current file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Traverse up to the root directory
-package_root = os.path.dirname(os.path.abspath(__file__))
-
-# Loop upwards until you reach the directory containing __init__.py
-while not os.path.exists(os.path.join(package_root, '__init__.py')):
-    package_root = os.path.dirname(package_root)
-
-print(package_root)
+def get_xml_columns(path, **kwargs) :
+	"""
+	Enter path and dictionary containing keys (must be column name from d3sensordefs file) and values for thoses keys
+	"""
+	ds = pd.read_csv(SES_PATH.xml_data_columns)
+	values = {**kwargs}
+	mask = np.all([ds[key] == values[key] for key in values.keys()], axis=0)
+	num_name = ds['number'].to_numpy()[mask]
+	tree = ET.parse(path)
+	root = tree.getroot()
+	chans_element = root.find(".//CHANS[@N='18']")
+	if chans_element is not None:
+		chans_numbers = list(map(int, chans_element.text.split(',')))
+		chans_array = np.array(chans_numbers)
+	else :
+		return 'Column number names were not found'
+	first_occurrences = [np.where(chans_array == num)[0][0] for num in num_name]
+	return first_occurrences
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
