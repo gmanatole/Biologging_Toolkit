aa77f344fac8b7c8abde73b7c655ef45b666d5b2
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-09-01 00:06:35 +0200

    Added inertial method to estimate speed

--- src/SES_tags/applications/Speed.py
+++ src/SES_tags/applications/Speed.py
@@ -9,33 +9,37 @@ from SES_tags.wrapper import Wrapper
 
 class Speed(Wrapper) :
 
-	def __init__(self):
-		pass
+	def __init__(self, depid, *, path):
+		
+		super().__init__(
+			depid,
+			path
+        )
 
-	def from_inertial(ml, freq = 60):
-		timestamps = pd.read_csv(f'/run/media/grosmaan/LaCie/individus_brut/individus/{ml}/welch_timestamps.csv')
-		inertial = pd.read_csv(f'/run/media/grosmaan/LaCie/individus_brut/individus/{ml}/inertial_data.csv')
-		spl_time, spl = [],[]
-		for file in timestamps.fn:
-			columns = pd.read_csv(file, nrows=0).columns.to_numpy()[:-1]
-			_freq = columns[np.argmin(abs(columns.astype(float)-freq))]
-			_temp = pd.read_csv(file, usecols = ['time', _freq])
-			spl_time.extend(_temp['time']) ; spl.extend(_temp[_freq])
-		dt = spl_time[1] - spl_time[0]; N=int(inertial.epoch.iloc[1]-inertial.epoch.iloc[0])
+	def _call__(self, overwrite = False) :
+		self.forward()
 		
-		spl_mean = np.convolve(spl, np.ones(3)/3, mode='valid')
-		spl_time_mean = np.convolve(spl_time, np.ones(3)/3, mode='valid')
-		_spl = interp1d(spl_time_mean, spl_mean, bounds_error=False)(inertial.epoch)
-		inertial[_freq] = np.log10(_spl)
+	def forward(self, overwrite = False, acoustic = True):
 		
-		angle = abs(inertial.elevation_angle.to_numpy())
+		self.from_inertial()
+		if acoustic :
+			self.from_acoustic()
+		
+		
+	def from_inertial(self):
+		"""
+		Method to compute speed based on vertical speed and elevation angle.
+		Moments where movement is in the horizontal plane are removed.
+		"""
+		angle = abs(self.ds['elevation_angle'].to_numpy())
 		angle[angle < 0.35] = np.nan
 
-		depth = inertial.depth.to_numpy()
-		dP = abs(depth[1:] - depth[:-1])
+		dP = abs(self.ds['depth'][:][1:] - self.ds['depth'][:][:-1])
 		dP[dP < 0.5] = np.nan
 		
-		speed = dP / np.sin(angle[:-1]) / N
-		inertial['speed'] = np.append(speed, speed[-1])
+		speed = dP / np.sin(angle[:-1]) / self.ds.dt
+		self.inertial_speed = np.append(speed, speed[-1])
 		
 
+	def from_acoustic(self, freq = 60):
+		pass
