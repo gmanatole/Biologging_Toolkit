9b42b6b058a3917000f42919abeaa5bd257fb94d
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-09-13 00:42:31 +0200

    Adapted code to class structure and to inherit wrapper

--- src/Biologging_Toolkit/applications/Drift_Dives.py
+++ src/Biologging_Toolkit/applications/Drift_Dives.py
@@ -5,38 +5,120 @@ import numpy as np
 import pandas as pd
 import umap.umap_ as umap
 import hdbscan
-import imageio.v3 as iio
-from sklearn.manifold import TSNE
 import scipy
-import netCDF4 as nc
-import time, datetime, calendar
+from tqdm import tqdm
 from Biologging_Toolkit.wrapper import Wrapper
 
 class DriftDives(Wrapper) :
+	"""
+	A class for analyzing drift dives in animals.
+	
+	Attributes:
+		depid: The deployment ID of the animal being analyzed.
+		analysis_length: The length of the dive portion (in seconds) that is analyzed to be considered as a drift dive.
+	"""
+	
+	def __init__(self, 
+			  depid, 
+			  *, 
+			  path,
+			  analysis_length = 60
+			  ):
+		"""
+		Initializes the DriftDives object with the given deployment ID, dataset path, and analysis length.
+		
+		Args:
+			depid: The deployment ID of the animal being analyzed.
+			path: The path to the dataset, must contain either inertial or depth data.
+			analysis_length: The length in seconds of the dive portion to be analyzed as a drift dive. Default is 60 seconds.
+		"""
 
-	def __init__(self) :
-		pass
+		super().__init__(
+			depid,
+			path
+			)
+		self.analysis_length = analysis_length
 
-	def from_inertial(self, inertial, len_analysis = 60, method = 'depth'):
-		'''
-		:param inertial: inertial dataframe with epoch, depth and bank_angle
-		:param len_analysis: length in seconds of dive portion that is analyzed to be considered as drift dive, defaults to 60
-		:return: DataFrame with dive type column
-		'''
-		len_analysis = int(len_analysis/3/2)
-		dive_type = np.full(len(inertial), 0)
-		for j in range(len_analysis, len(inertial)-len_analysis-1) :
-			if ((abs(inertial.bank_angle.iloc[j-len_analysis:j+len_analysis].to_numpy()) > 2).sum() / len(inertial.iloc[j-len_analysis:j+len_analysis]) > 0.98):
+	def __call__(self, overwrite = False, mode = 'inertial') :
+		self.forward(mode, overwrite)
+		
+	def forward(self, mode = 'inertial', overwrite = False):
+		"""
+		Calls the  correct method for drift dive identification in the specified mode.
+		Updating the dataset with drift information.
+		
+		Args:
+			mode: The mode of analysis. Can be 'inertial' (uses bank angle) or 'depth' (uses vertical speed). Default is 'inertial'.
+			overwrite: If True, existing drift variables in the dataset will be removed before analysis. Default is False.
+		Mode-specific behavior:
+			- 'inertial': Drift portions are flagged where the animal has a bank angle greater than 2 radians for 98% of the analysis length.
+			- 'depth': Drift portions are flagged where the animal's vertical speed is below 0.4 m/s for the analysis length.
+		"""
+		if mode == 'inertial' :
+			self.from_inertial()
+			if overwrite :
+				if 'inertial_drift' in self.ds.variables:
+					self.remove_variable('inertial_drift')
+	
+			if 'inertial_speed' not in self.ds.variables:
+				inertial = self.ds.createVariable('inertial_drift', np.float64, ('time',))
+				inertial.units = 'binary'
+				inertial.long_name = 'Flag indicating whether or not the animal is passively drifting'
+				inertial.measure = '1 for drift portions, 0 for any other behavior'
+				inertial.analysis_length = self.analysis_length
+				inertial.note = 'Dive portions where elephant seal has bank angle greater than 2 rad for 98% of analysis length'
+				inertial[:] = self.inertial_drift		
+				
+		if mode == 'depth' :
+			self.from_inertial()
+			if overwrite :
+				if 'depth_drift' in self.ds.variables:
+					self.remove_variable('depth_drift')
+	
+			if 'depth_speed' not in self.ds.variables:
+				depth = self.ds.createVariable('depth_drift', np.float64, ('time',))
+				depth.units = 'binary'
+				depth.long_name = 'Flag indicating whether or not the animal is passively drifting'
+				depth.measure = '1 for drift portions, 0 for any other behavior'
+				depth.analysis_length = self.analysis_length
+				depth.note = 'Dive portions where elephant seal has average vertical speed below 0.4 m/s during analysis length'
+				depth[:] = self.depth_drift
+
+
+	def from_inertial(self):
+		"""
+		Identifies drift portions based on the animal's bank angle ('inertial' mode).
+		
+		Analyzes sections of the dive where the bank angle exceeds 2 radians for more than 98% of the analysis length.
+		The results are stored in `inertial_drift`, a binary flag in the dataset.
+		"""
+		idx_bound = int(self.analysis_length / self.ds.sampling_rate / 2)
+		dive_type = np.full(len(self.ds['bank_angle'][:]), 0)
+		dive_type = np.full(len(self.ds['depth'][:]), 0)
+		pbar = tqdm(total = len(dive_type)-2*idx_bound-1, position = 0, leave = True)
+		pbar.set_description('Iterating through dataset')
+		for j in tqdm(range(idx_bound, len(dive_type)-idx_bound-1)) :
+			if ((abs(self.ds['bank_angle'][:][j-self.analysis_length:j+self.analysis_length]) > 2).sum() / self.analysis_length > 0.98):
 				dive_type[j] =  1 
+		self.inertial_drift = dive_type
 
-	def from_depth(self, depth):
-		len_analysis = int(len_analysis/3)
-		dive_type = np.full(len(inertial), 0)
-		for j in range(len_analysis, len(inertial)-len_analysis-1) :
-			vertical_speed_before = (inertial.depth.iloc[j] - inertial.depth.iloc[j-len_analysis])/(inertial.epoch.iloc[j] - inertial.epoch.iloc[j-len_analysis]) 
-			vertical_speed_after = (inertial.depth.iloc[j+len_analysis] - inertial.depth.iloc[j])/(inertial.epoch.iloc[j+len_analysis] - inertial.epoch.iloc[j]) 
+	def from_depth(self):
+		"""
+		Identifies drift portions based on the animal's vertical speed ('depth' mode).
+		
+		Analyzes sections of the dive where the average vertical speed is below 0.4 m/s over the analysis length.
+		The results are stored in `inertial_drift`, a binary flag in the dataset.
+		"""
+		idx_bound = int(self.analysis_length / self.sampling_rate)
+		dive_type = np.full(len(self.ds['depth'][:]), 0)
+		pbar = tqdm(total = len(dive_type)-2*idx_bound-1, position = 0, leave = True)
+		pbar.set_description('Iterating through dataset')
+		for j in tqdm(range(idx_bound, len(dive_type)-idx_bound-1)) :
+			vertical_speed_before = (self.ds['depth'][:][j] - self.ds['depth'][:][j-idx_bound])/(self.ds['time'][:][j] - self.ds['time'][:][j-idx_bound]) 
+			vertical_speed_after = (self.ds['depth'][:][j+idx_bound] - self.ds['depth'][:][j])/(self.ds['time'][:][j+idx_bound] - self.ds['time'][:][j]) 
 			if (abs(vertical_speed_after) < 0.4) or abs((vertical_speed_before) < 0.4) :
 				 dive_type[j] = 1
+		self.depth_drift = dive_type
 
 
 	def acoustic_cluster(self, ml, var = '2200', min_cluster_size = 20, min_samples = 15):
