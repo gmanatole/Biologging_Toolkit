d2339eb9dd6803f2e8ba43f042d25a529f0339a4
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-09-13 13:28:23 +0200

    Import all utils through import utils

---
+++ src/Biologging_Toolkit/utils/__init__.py
@@ -0,0 +1,5 @@
+from Biologging_Toolkit.utils.format_utils import *
+from Biologging_Toolkit.utils.acoustic_utils import *
+from Biologging_Toolkit.utils.inertial_utils import *
+
+__all__ = [name for name in dir() if not name.startswith('_')]
