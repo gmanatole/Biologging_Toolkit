b9242eb2f110d7dd4f9d630c2964ec4a5bc74c8c
Anatole GROS-MARTIAL <osmose@datarmor.com>
2024-10-07 22:03:19 +0000

    Fixed minor bugs

--- src/Biologging_Toolkit/deep_learning/.ipynb_checkpoints/Wind_LSTM-checkpoint.py
+++ src/Biologging_Toolkit/deep_learning/.ipynb_checkpoints/Wind_LSTM-checkpoint.py
@@ -13,27 +13,29 @@ class WindLSTM() :
 			  path : str = None,
 			  acoustic_path : str = None,
 			  variable : str = 'era',
-			  *,
-			  num_epochs = 25,
-			  learning_rate = 0.001,
-			  weight_decay = 0.000,
-			  criterion : str = 'MSE'
+              *,
+              num_epochs = 25,
+              learning_rate = 0.001,
+              weight_decay = 0.000,
+              criterion : str = 'MSE'
 			  ) :
 		
 		self.depid = depid
 		self.acoustic_path = acoustic_path
 		self.ds = nc.Dataset(path)
 		self.fns = find_npz_for_time(self.ds['time'][:], self.acoustic_path)
+		self.dives = self.ds['dives'][:].data
+		self.depth = self.ds['depth'][:].data
 		self.variable = variable
 
-		self.learning_rate = learning_rate
-		self.weight_decay = weight_decay
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
 		self.model = RNNModel(1, 512, 1, 1)
-		self.dataloader = utils.data.DataLoader(LoadData(self.variable, self.ds, self.fns))
-		if criterion == 'MSE':
-			self.criterion = nn.MSELoss()
+		self.dataloader = LoadData(self.variable, self.fns, self.dives, self.depth)
+        if criterion == 'MSE':
+    		self.criterion = nn.MSELoss()
 		self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate, weight_decay = 0.000)
-		self.num_epochs = num_epochs
+        self.num_epochs = num_epochs
 
 	def train(self):
 	
@@ -55,7 +57,7 @@ class WindLSTM() :
                 
 			accuracy.append(np.mean(acc_batch))
 
-			self.test(epoch)
+            self.test(epoch)
 			self.model.train()
 
 			torch.save(model.state_dict(), 'current_training')
@@ -77,30 +79,26 @@ class WindLSTM() :
 				acc_test.append(np.sum(abs(outputs-labels) < 1)/len(labels))	
 		np.save('results/'+str(epoch+1), np.vstack((all_preds, all_labels)))
 
-class LoadData(utils.data.Dataset) :
+class LoadData(utils.data.DataLoader) :
 	
 	seq_length = 500
 	
-	def __init__(self, variable, ds, fns):
-		self.fns = fns
-		self.ds = ds
-		self.dives = self.ds['dives'][:].data
+	def __init__(self, variable, files, dives, depth):
+		self.files = files
 		self.variable = variable 
-		self.label = self.ds[variable][:].data
+		self.dives = dives
+		self.depth = depth
 
 	def __len__(self):
 		'''
 		Returns the number of dives in dataset
 		'''
-		return len(np.unique(self.dives))
+		return len(np.unique(self.dives)[0])
 	
 	def __getitem__(self, idx):
-		mask = self.dives == idx
-		label = self.label[mask][-1]
-		_data = np.load(self.fns[mask][0])
-		pos = np.searchsorted(_data['time'], self.ds['time'][mask].data)
-		spectro = _data['spectro'][pos]
-		spectro = torch.Tensor(spectro)
+		#TRY USING X ARRAY LAZY LOADING
+		label = self.variable[self.dives == idx][-1]
+		spectro = torch.Tensor(self.acoustic[:].data[self.dives == idx])
 		
 		if len(spectro) == self.seq_length:
 			return spectro, torch.tensor(label, dtype=torch.float)
