cea117f30d8fa910ce54c72222a56e2d6d5fcba0
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-10-07 17:49:31 +0200

    Added RNN Class model

--- src/Biologging_Toolkit/deep_learning/Wind_LSTM.py
+++ src/Biologging_Toolkit/deep_learning/Wind_LSTM.py
@@ -23,7 +23,7 @@ class WindLSTM() :
 		self.depth = self.ds['depth'][:].data
 		self.variable = variable
 
-		self.model = 'RNN'
+		self.model = RNNModel(1, 512, 1, 1)
 		self.dataloader = LoadData(self.variable, self.fns, self.dives, self.depth)
 		self.criterion = nn.MSELoss()
 		self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001, weight_decay = 0.000)
@@ -102,3 +102,39 @@ class LoadData(utils.data.DataLoader) :
 		else:
 			spectro = torch.cat((torch.zeros(self.seq_length-len(spectro), spectro.size(1)), spectro))
 			return spectro, torch.tensor(label, dtype=torch.float)
+
+
+class RNNModel(nn.Module):
+	'''
+	RNN module that can implement a LSTM
+	Number of hiddend im and layer dim are parameters of the model
+	'''
+	def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+		super(RNNModel, self).__init__()
+
+		# Number of hidden dimensions
+		self.hidden_dim = hidden_dim
+
+		# Number of hidden layers
+		self.layer_dim = layer_dim
+
+		# RNN
+		#self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='relu')
+		self.rnn = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+
+		self.fc1 = nn.Linear(hidden_dim, 128)
+
+		# Readout layer
+		self.fc = nn.Linear(128, output_dim)
+
+	def forward(self, x):
+
+		# Initialize hidden state with zeros
+		h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+		c0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+		#pdb.set_trace()
+		out, (hn, cn) = self.rnn(x, (h0, c0))
+		out = self.fc1(out[:, -1, :])
+		out = self.fc(out)
+
+		return out
