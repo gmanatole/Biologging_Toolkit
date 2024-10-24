183a721d2f613f82113f74b650744f2891a74d66
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-10-24 14:08:49 +0200

    Modified filter to get better predictions

--- src/Biologging_Toolkit/applications/Waves.py
+++ src/Biologging_Toolkit/applications/Waves.py
@@ -2,6 +2,7 @@ import os
 from glob import glob
 import numpy as np
 from scipy.signal import welch, find_peaks, butter, filtfilt, sosfilt
+from scipy.ndimage import median_filter
 import netCDF4 as nc
 from datetime import datetime, timezone
 import soundfile as sf
@@ -72,8 +73,8 @@ class Waves(Dives) :
 		period, period_time, med_period, zc_period = [], [], [], []
 		
 		#Get parameters for pass band filter
-		params = butter(N=4, fs = samplerate, Wn=[1/12, 1/5], btype='bandpass', analog=False, output = 'sos')
-
+		params = butter(N=4, fs = samplerate, Wn=[1/15, 1/3], btype='bandpass', analog=False, output = 'sos')
+		
 		surface_mask, surface_periods = self.get_surface(self.P, 2)
 		for i in np.unique(surface_periods) :
 			surface = surface_periods == i
@@ -81,11 +82,11 @@ class Waves(Dives) :
 			fn_idx = np.argmax(xml_start_time[xml_start_time - surface_time[0] < 0])
 			fs = sf.info(swv_fns[fn_idx]).samplerate
 			sig, fs = sf.read(swv_fns[fn_idx], 
-					 start = int((surface_time[0] - xml_start_time[fn_idx] - 1)*fs),
-					 stop = int((surface_time[-1] - xml_start_time[fn_idx] + 1)*fs))
+					 start = int((surface_time[2] - xml_start_time[fn_idx] - 1)*fs),
+					 stop = int((surface_time[-3] - xml_start_time[fn_idx] + 1)*fs))
 			A_surf = np.column_stack([sig[:,idx_names[i]].flatten() for i in range(len(idx_names))])
 			A_surf = (A_surf * self.A_cal_poly[0] + self.A_cal_poly[1]) @ self.A_cal_map
-			
+
 			ea = np.nanmean(elevation_angle[(time_angle > surface_time[0]) & (time_angle < surface_time[-1])])
 			A_filtered = sosfilt(params, A_surf[:,0]*np.sin(ea) + A_surf[:,2]*np.cos(ea))
 
@@ -97,7 +98,7 @@ class Waves(Dives) :
 			
 			# Find frequency using zero crossings
 			zero_crossings = np.where(np.diff(np.sign(A_filtered)))[0]
-			_zc_period = np.nanmedian(np.diff(zero_crossings))/samplerate
+			_zc_period = np.nanmedian(np.diff(zero_crossings))/samplerate*2
 			
 			# Find peaks using FFT
 			spectrum = np.abs(np.fft.fft(A_filtered))
@@ -105,12 +106,13 @@ class Waves(Dives) :
 			psd_peaks, _ = find_peaks(spectrum[freqs > 0], threshold = 0.01)
 			try :
 				peak = np.argmax(spectrum[freqs > 0][psd_peaks])
-				period.append(1 / freqs[freqs > 0][peak])
+				period.append(1 / freqs[freqs > 0][psd_peaks[peak]])
 			except ValueError:
 				period.append(np.nan)
 			period_time.append(np.mean(surface_time))
 			med_period.append(_med_period)
 			zc_period.append(_zc_period)
+
 		self.zc_period = np.array(zc_period)
 		self.period = np.array(period)
 		self.period_time = np.array(period_time)
