04a43e0b1c6e48fafc60caa70ec520d298c77284
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2024-10-29 20:59:05 +0100

    Added threshold method to MLD code

--- src/Biologging_Toolkit/applications/Mixed_Layer_Depth.py
+++ src/Biologging_Toolkit/applications/Mixed_Layer_Depth.py
@@ -9,7 +9,9 @@ class MixedLayerDepth(Dives) :
 	def __init__(self,
 			  depid : str,
 			  *,
-			  path : str = None  
+			  path : str = None  ,
+			  threshold = 0.2,  #0.2 from Dong, 2008
+			  criterion = 0.3
 			  ):
 		
 				
@@ -21,11 +23,27 @@ class MixedLayerDepth(Dives) :
 		self.depth = self.ds['depth'][:].data
 		self.temp = self.ds['temperature'][:].data
 		self.dives = self.ds['dives'][:].data
-		self.criterion = 0.03
-		
+		self.criterion = criterion
+		self.threshold = threshold
+
+
 	def __call__(self, overwrite = False):
 		return self.forward(overwrite = overwrite)
 	
+	def forward(self, overwrite = True):
+		self.get_threshold_mld()
+		self.threhsold_mld = np.array(self.threshold_mld).reshape(-1,2)
+		self.get_relative_variance_mld()
+		
+		self.dive_ds['zn1_up'] = np.array(self.zn1)[:,1]
+		self.dive_ds['zn2_up'] = np.array(self.zn2)[:,1]
+		self.dive_ds['zn1_down'] = np.array(self.zn1)[:,0]
+		self.dive_ds['zn2_down'] = np.array(self.zn2)[:,0]
+		self.dive_ds['threhsold_up'] = self.threhsold_mld[:,0]
+		self.dive_ds['threhsold_down'] = self.threhsold_mld[:,1]
+		self.dive_ds.to_csv(self.dive_path, index = None)
+		
+	'''
 	def forward(self, window_size = 10, overwrite = True):
 		self.compute_mld()
 		zn1_corr, zn2_corr = [], []
@@ -44,33 +62,76 @@ class MixedLayerDepth(Dives) :
 		self.zn2_smoothed = zn2_series.rolling(window=window_size, min_periods=1).mean().to_numpy()
 		self.dive_ds['zn1'] = self.zn1_smoothed
 		self.dive_ds['zn2'] = self.zn2_smoothed
-		self.dive_ds.to_csv(self.dive_path, index = None)
-	
+		self.dive_ds.to_csv(self.dive_path, index = None)'''
 	
 	@staticmethod
-	def profile_check(depth, profile, sr) :
-		if len(profile[depth > 10]) >= 3 * 60 / sr :
-			indices = np.argsort(depth)
-			depth = depth[indices]
-			profile = profile[indices]
-			return depth[depth > 10], profile[depth > 10]
-		else :
+	def profile_check(depth, profile) :
+		indices = np.argsort(depth)
+		depth = depth[indices]
+		profile = profile[indices][depth > 10]
+		depth = depth[depth > 10]
+		if len(depth) == 0 :
 			return [], []
+		if np.max(depth) < 50 :
+			return [], []
+		if np.min(depth)  > 20 :
+			return [], []
+		if abs(np.mean(np.diff(depth))) > 6 :  #Changed from dz = 2 to dz = 6 as most dz for SES are below 5 (criteria for consistency of profiles)
+			return [], []
+		if len(depth[(depth >= 10) & (depth <= 40)]) < 2 :
+			return [], []
+		if np.max(profile[depth <= 20]) - np.min(profile[depth <= 20]) >= np.max(profile[depth >= 20]) - np.min(profile[depth >= 20]) :
+			return [], []
+		if np.max(profile) - np.min(profile) < 1 :
+			return [], []
+		return depth, profile
+	
+	@staticmethod
+	def threshold_profile_check(depth, profile) :
+		indices = np.argsort(depth)
+		depth = depth[indices]
+		profile = profile[indices]
+		return depth[depth > 10], profile[depth > 10]  #Depth to keep from Montegut, 2004 is 10 m whereas Dong, 2008 uses 20m
+
+	
+	def get_threshold_mld(self) :
+		if self.up is None or self.down is None :
+			self.dive_profile()
+			
+		self.threshold_mld = []
+		unique_dives = np.unique(self.dives)
+		#Look at each dive individually
+		for dive in tqdm(unique_dives, desc = 'Computing threshold MLD for each dive') :
+			# Get estimations from two profiles : up and down trajectories
+			for direction in [self.up, self.down]:		
+				_temp = self.temp[(self.dives == dive) & direction]
+				_depth = self.depth[(self.dives == dive) & direction]
+				_depth, _temp = self.threshold_profile_check(_depth, _temp)
+
+				i = 0
+				while (i < len(_temp)) and (abs(_temp[i] - _temp[0]) < self.threshold):
+					i+=1
+				if i == len(_temp) or abs(_temp[i] - _temp[0]) > 0.7 :  #Flag criterion from Montegut, 2004
+					self.threshold_mld.append(np.nan)
+				else :
+					self.threshold_mld.append(_depth[i])
+				
+	def get_relative_variance_mld(self):
 		
-	def compute_mld(self):
-		up, down = self.get_dive_direction(self.depth[:: 60 // self.ds.sampling_rate])
-		up, down = resample_boolean_array(up, len(self.depth)), resample_boolean_array(down, len(self.depth))
+		if self.up == None or self.down == None :
+			self.dive_profile()
+			
 		self.zn2, self.zn1 =[], []
 		unique_dives = np.unique(self.dives)
 		#Look at each dive individually
 		for dive in tqdm(unique_dives, desc = 'Computing MLD for each dive') :
 			_zn1, _zn2 = [], []
 			# Get estimations from two profiles : up and down trajectories
-			for direction in [up, down]:
+			for direction in [self.up, self.down]:
 				_temp = self.temp[(self.dives == dive) & direction]
 				_depth = self.depth[(self.dives == dive) & direction]
 				#Sort temp and depth profile by increasing depth and check if there is enough data
-				_depth, _temp = self.profile_check(_depth, _temp, self.ds.sampling_rate)
+				_depth, _temp = self.profile_check(_depth, _temp)
 				delta_k, std_k, X_k = [],[],[]
 				for k in range(len(_temp)):
 					delta_k.append(np.sqrt( 1/(k+1) * np.nansum([((_temp[i] - np.nanmean(_temp[:k+1]))**2) for i in range(k+1)])))
@@ -84,10 +145,12 @@ class MixedLayerDepth(Dives) :
 					_zn1.append(np.nan)
 					_zn2.append(np.nan)
 					continue
+				_zn2_temp = np.nan
 				for step in range(len(delta_k[:pos_zn1 - 1]), -1, -1):
 					if (delta_k[step + 1] - delta_k[step]) / (delta_k[pos_zn1 + 1] - delta_k[pos_zn1]) <= self.criterion :
-						_zn2.append(_depth[step])
+						_zn2_temp = _depth[step]
 						break
+				_zn2.append(_zn2_temp)
 			self.zn2.append(_zn2)
 			self.zn1.append(_zn1)
 		
