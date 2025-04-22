c57812e684f5dbdaab9b5b919ac8d4d825cd09cb
Anatole GROS-MARTIAL <anatole.gros-martial@ensta.fr>
2025-04-22 18:18:50 +0200

    Variable name change in sun position function

--- src/Biologging_Toolkit/auxiliary/Sun.py
+++ src/Biologging_Toolkit/auxiliary/Sun.py
@@ -1,12 +1,17 @@
+import coord
+import numpy as np
+from astropy.time import Time
+import astropy.units as u
+
 def azimuth_to_trigonometric(azimuth):
     trigonometric_angle = 450 - azimuth
     trigonometric_angle = trigonometric_angle % 360
     return trigonometric_angle
 
-def get_sun_pos(aux):
-	loc_ses = coord.EarthLocation(lat = aux.lat * u.deg, lon = aux.lon * u.deg, height = 0 * u.m)
-	time_ses = Time(aux.time, format='unix', scale = 'utc')
-	sun = coord.get_sun(time_ses)
-	altaz = coord.AltAz(location=loc_ses, obstime=time_ses)
+def get_sun_pos(timestamp, lat, lon):
+	loc_ses = coord.EarthLocation(lat = lat * u.deg, lon = lon * u.deg, height = 0 * u.m)
+	timestamp = Time(timestamp, format='unix', scale = 'utc')
+	sun = coord.get_sun(timestamp)
+	altaz = coord.AltAz(location=loc_ses, obstime=timestamp)
 	return np.array(sun.transform_to(altaz).az)*np.pi/180, np.array(sun.transform_to(altaz).zen)*np.pi/180
 	
