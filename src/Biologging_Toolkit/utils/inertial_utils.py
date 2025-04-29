9189794870b584f0d57696d47265cdcb1ef5b1e6
Anatole GROS-MARTIAL <grosmartial.anatole@gmail.com>
2025-04-29 17:26:12 +0200

    Euler function angle

--- src/Biologging_Toolkit/utils/inertial_utils.py
+++ src/Biologging_Toolkit/utils/inertial_utils.py
@@ -3,6 +3,14 @@ import numpy as np
 import requests
 import xml.etree.ElementTree as ET
 
+def find_sequence(condition, samplerate =  1):
+    condition = condition.astype(int)
+    edges = np.diff(condition, prepend=0, append=0)
+    start_idx = np.where(edges == 1)[0]
+    end_idx = np.where(edges == -1)[0] - 1
+    length = (end_idx - start_idx + 1) / samplerate
+    result = np.vstack([condition[start_idx], start_idx, end_idx, length]).T
+    return result
 
 def norm(X):
 	m, n = X.shape
